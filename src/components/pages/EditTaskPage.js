9e60a122599d339d4dacdfa08cb62f5ccb83f03d
yehorparkhomenko <yehorparkhomenko@gmail.com>
2021-09-16 13:25:11 +0300

    Fix errors

--- src/components/pages/EditTaskPage.js
+++ src/components/pages/EditTaskPage.js
@@ -30,6 +30,17 @@ class EditTaskPage extends React.Component {
     
     if(response["status"] == ["ok"]) {
       message.success('Задача изменена!')
+
+      const task = this.props.location.state.task
+      task['status'] = values['status']
+      task['username'] = values['username']
+      task['email'] = values['email']
+      task['text'] = values['text']
+
+      this.props.history.push({
+        pathname: '/edit',
+        state: { task: task }
+      })
     } else {
       message.error(buildErrorsText(response["message"]))
     }
